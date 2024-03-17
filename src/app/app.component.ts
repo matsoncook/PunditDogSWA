041efc1cee71c4ae7c135b914189b89a014ea0c0
Mark Cook <markcook2001@gmail.com>
2024-03-18 11:29:06 +1300

    second

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+//import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,MatInputModule,MatFormFieldModule,
+    //BrowserAnimationsModule,
+    FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'PunditDogSWA';
+  webAddress = "hello";
+  apiCode = "sdfsdfhbfdhbf==";
+  webCall = this.webAddress + "?code=" + this.apiCode;
 }
