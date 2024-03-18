db85bf7db814d46418d25b7ec9b0b429fe616fe3
matsoncook <markcook2001@gmail.com>
2024-03-18 08:31:55 +0000

    Stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CommonModule, NgIf} from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
@@ -8,41 +9,58 @@ import { HttpClientModule,HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+
+
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatInputModule, MatFormFieldModule,
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule,NgIf,
     //BrowserAnimationsModule,
-    FormsModule,MatButtonModule,MatIconModule,HttpClientModule],
+    FormsModule,MatButtonModule,MatIconModule,HttpClientModule,MatProgressSpinnerModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+  isLoading= false;
   title = 'PunditDogSWA';
-  webAddress = "hello";
-  apiCode = "sdfsdfhbfdhbf==";
+  webAddress = "https://punditdogfws.azurewebsites.net/api/HttpExample";
+  apiCode = "";
   //webCall = this.webAddress + "?code=" + this.apiCode;
-  webCall = '';
+  webCall = "";
 
   webServiceResponse = "";
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) 
+  {
+    this.compileWebCall();
+  }
 
   onSubmit() {
     console.log('Form submitted');
     this. webseviceSubmit();
+    this.compileWebCall();
+  }
+
+  compileWebCall()
+  {
+    this.webCall = this.webAddress + "?code=" + this.apiCode;
+    console.log(this.webCall);
   }
 
   webseviceSubmit()
   {
+    this.isLoading = true;
     this.http.get(this.webCall).subscribe(data => {
       console.log(data);
       this.webServiceResponse = JSON.stringify(data,null,2);
+      this.isLoading = false;
     }, error => {
       console.error(error);
+      this.isLoading = false;
     });
   }
 }
