6095a09457633b8cecd07078a4c064745d9a225f
matsoncook <markcook2001@gmail.com>
2024-03-18 02:27:26 +0000

    http request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,14 +4,19 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 //import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
+import { HttpClientModule,HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,MatInputModule,MatFormFieldModule,
+  imports: [RouterOutlet, MatInputModule, MatFormFieldModule,
     //BrowserAnimationsModule,
-    FormsModule],
+    FormsModule,MatButtonModule,MatIconModule,HttpClientModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -19,5 +24,22 @@ export class AppComponent {
   title = 'PunditDogSWA';
   webAddress = "hello";
   apiCode = "sdfsdfhbfdhbf==";
-  webCall = this.webAddress + "?code=" + this.apiCode;
+  //webCall = this.webAddress + "?code=" + this.apiCode;
+  webCall = 'https://www.google.com';
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    console.log('Form submitted');
+    this. webseviceSubmit();
+  }
+
+  webseviceSubmit()
+  {
+    this.http.get(this.webCall).subscribe(data => {
+      console.log(data);
+    }, error => {
+      console.error(error);
+    });
+  }
 }
